6d966c077afcdf059b6c448dabf5b1290b9668e4
abdellah nejmeddine <crissnajm1998@gmail.com>
2023-08-17 01:24:55 +0100

    update 8

--- src/main/java/ma/azdad/view/AppraisalsView.java
+++ src/main/java/ma/azdad/view/AppraisalsView.java
@@ -337,7 +337,7 @@ public class AppraisalsView extends GenericView<Integer, Appraisals, AppraisalsR
 
 	public List<User> getUsersByManager() {
 
-		users = appraisalsService.findByHr(true, true, sessionView.getUser());
+		users = appraisalsService.findByHr(sessionView.getUser());
 
 		return users;
 	}
